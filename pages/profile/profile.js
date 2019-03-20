c68b2321e48152f6f40f5eb19cf0d1453495bb3a
wuynje <wuynje@163.com>
2019-03-20 17:25:25 +0800

    点击触发登录请求

--- pages/profile/profile.js
+++ pages/profile/profile.js
@@ -1,23 +1,29 @@
-// 获取全局应用程序实例对象
+//获取应用实例
 const app = getApp()
-
-// 创建页面实例对象
 Page({
-  /**
-   * 页面的初始数据
-   */
   data: {
-    title: 'About',
-    userInfo: {
-      nickName: '汪磊(iceStone)',
-      avatarUrl: '../../images/qrcode.png'
-    }
-  },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad () {
-    console.log("测试")
+  },
+  //登录获取code
+  userinfoget: function () {
+    wx.login({
+      success: function (res) {
+        console.log('code:' + res.code)
+        //发送请求
+        wx.request({
+          url: 'http://localhost:8085/login', //改成自己的服务器地址
+          data: {
+            code: res.code,//上面wx.login()成功获取到的code
+            operFlag: 'getOpenid',
+          },
+          header: {
+            'content-type': 'application/json' //默认值
+          },
+          success: function (res) {
+            console.log(res)
+          }
+        })
+      }
+    })
   }
 })
